d906734054d967ba84d5d13a772a006ca50ac4ae
n_knuu <premier3next@gmail.com>
2015-06-17 20:08:47 +0900

    edit .emacs.d(add some packages)
    markdown-mode, gitignore-mode, gitconfig-modeを入れた

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -186,6 +186,12 @@
 ;; quickrun
 (require 'quickrun)
 
+;; markdown-mode
+(autoload 'markdown-mode "markdown-mode"
+   "Major mode for editing Markdown files" t)
+;(add-to-list 'auto-mode-alist '("\\.text\\'" . markdown-mode))
+(add-to-list 'auto-mode-alist '("\\.markdown\\'" . markdown-mode))
+(add-to-list 'auto-mode-alist '("\\.md\\'" . markdown-mode))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;; shell (TODO!!)
