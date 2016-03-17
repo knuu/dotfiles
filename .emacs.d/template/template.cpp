01a5d83926fc29fba164daf2f20f63feb2ac94a7
n_knuu <premier3next@gmail.com>
2016-03-17 09:19:11 +0900

    edit c++ template
    変数Tをたまに使うのでtemplateを変更

--- .emacs.d/template/template.cpp
+++ .emacs.d/template/template.cpp
@@ -5,11 +5,14 @@ typedef long long int ll;
 typedef pair<int, int> P;
 typedef pair<ll, ll> Pll;
 typedef vector<int> Vi;
-typedef tuple<int, int, int> T;
+//typedef tuple<int, int, int> T;
 #define FOR(i,s,x) for(int i=s;i<(int)(x);i++)
 #define REP(i,x) FOR(i,0,x)
 #define ALL(c) c.begin(), c.end()
 #define DUMP( x ) cerr << #x << " = " << ( x ) << endl
+
+const int dr[4] = {-1, 0, 1, 0};
+const int dc[4] = {0, 1, 0, -1};
 
 int main() {
   // use scanf in CodeForces!
